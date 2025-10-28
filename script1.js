96fc458dac337d40be5416c6096252f3f36e06b4
matthagtvedt-alt <matthagtvedt@gmail.com>
2025-10-28 13:58:10 +0100

    laget console.log får å eksperimentere med JS

---
+++ script1.js
@@ -0,0 +1,11 @@
+//Huntington-Hill Metoden//
+let resultat = 100000
+let dividend = 1
+resultat /= dividend*(dividend+1)**(1/2)
+
+//Setning//
+let quote = "My name is"
+quote += " " + "Dude"
+
+console.log(resultat)
+console.log(quote)
