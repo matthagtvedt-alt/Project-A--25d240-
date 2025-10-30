61eac1ecc2b71a18117a54eb744425614ae77c35
matthagtvedt-alt <matthagtvedt@gmail.com>
2025-10-30 13:55:43 +0100

    if og else på script filen

--- script1.js
+++ script1.js
@@ -1,11 +1,22 @@
-//Huntington-Hill Metoden//
+//Huntington-Hill Metoden
 let resultat = 100000
-let dividend = 1
-resultat /= dividend*(dividend+1)**(1/2)
+let divisor = 1
+resultat /= divisor*(divisor+1)**(1/2)
 
-//Setning//
+//Setning
 let quote = "My name is"
 quote += " " + "Dude"
 
 console.log(resultat)
 console.log(quote)
+
+//If og Else funksjoner
+let erAnsatt = false
+
+if(erAnsatt){
+    console.log("Du er ansatt")
+}
+else{
+    console.log("Du er IKKE ansatt")
+}
+
